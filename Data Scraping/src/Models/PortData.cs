d85229186de99f0b30261ba44fbfe5504994d944
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-07-11 00:13:13 +0700

    feat: add port data model

---
+++ Data Scraping/src/Models/PortData.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace BmkgScraper.Models;
+
+internal sealed class PortData
+{
+    [JsonPropertyName("port_code")]
+    public required string PortCode { get; init; }
+
+    [JsonPropertyName("port_name")]
+    public required string PortName { get; init; }
+
+    [JsonPropertyName("detail_url")]
+    public required string DetailUrl { get; init; }
+}
